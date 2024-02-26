88cd2fcf847f77e3ab4230a9ae86f9b227b13679
guest67 <1650202750@qq.com>
2024-02-25 22:41:10 -0330

    Importing models and simple viewpoint controling

--- vue_frontend/src/views/HomeView.vue
+++ vue_frontend/src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to skiing!"/>
+  <nav>
+    <router-link to="/map">Show Map</router-link>
+  </nav>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'HomeView',
---
+++ vue_frontend/src/views/MapView.vue
@@ -0,0 +1,184 @@
+<template>
+  <p>Camera:{{ cx }},{{ cy }},{{ cz }}, OrbitControls:{{ ox }},{{ oy }},{{ oz }}, Resize:{{ test }}</p>
+  <button @click="go">up</button>
+  <div class="container" ref="container"></div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import * as THREE from "three"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader"
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+
+const cx = ref(0);
+const cy = ref(0);
+const cz = ref(0);
+const ox = ref(0);
+const oy = ref(0);
+const oz = ref(0);
+const test = ref(0);
+// Initialize the scene 初始化场景
+const scene = new THREE.Scene();
+// Initialize the camera 初始化相机
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000)
+// Initialize the renderer 初始化渲染器
+const renderer = new THREE.WebGLRenderer({
+  antialias: true, // Anti-aliasing 抗锯齿
+  logarithmicDepthBuffer: true, // Logarithmic depth buffer 深度缓冲器
+})
+// 定义容器及控制器
+const container = ref()
+let controls: OrbitControls | null = null;
+
+// Initialize the environment
+const Initialize = () => {
+  // Set camera position 设置相机位置
+  camera.position.set(4, 11, -17);
+  scene.add(camera)
+  // Create a sky sphere 创建天空球
+  const skyGeo = new THREE.SphereGeometry(1000, 60, 60);
+  const skyTex = new THREE.TextureLoader().load("/images/sky.jpg")
+  const skyMat = new THREE.MeshBasicMaterial({
+    map: skyTex
+  })
+  skyGeo.scale(1, 1, -1)
+  const sky = new THREE.Mesh(skyGeo, skyMat)
+  scene.add(sky)
+
+  // // Video texture 动态天空球
+  // const createVideo = () => {
+  //   const video = document.createElement("video");
+  //   video.src = "/video/sky.mp4"
+  //   video.loop = true
+  //   window.addEventListener("click", (e) => {
+  //     if (video.paused) {
+  //       video.play();
+  //       const texture = new THREE.VideoTexture(video)
+  //       skyMat.map = texture
+  //       skyMat.map.needsUpdate = true;
+  //     }
+  //   })
+
+  // }
+
+  // Load environment texture 加载环境纹理
+  const hdrLoader = new RGBELoader()
+  hdrLoader.loadAsync("/hdr/050.hdr").then((texture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture
+    scene.environment = texture
+  })
+
+  // Add parallel light 设置平行光
+  const light = new THREE.DirectionalLight(0xffffff, 1)
+  light.position.set(-100, 100, 10)
+  scene.add(light)
+
+  // Add model 载入模型
+  const loader = new GLTFLoader();
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("/draco/")
+  loader.setDRACOLoader(dracoLoader)
+  loader.load("/glb/glbfile.glb", (gltf) => {
+    const isLand = gltf.scene
+    isLand.scale.set(100, 100, 100);
+    isLand.rotation.y = -Math.PI / 2; // Rotate 90 degrees
+    scene.add(isLand)
+  })
+}
+
+// 更新相机和渲染器
+const updateCameraAndRenderer = () => {
+  // Update camera's aspect ratio
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // Update camera's projection matrix
+  camera.updateProjectionMatrix();
+  // Update renderer's size
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// 渲染
+const render = () => {
+  renderer.render(scene, camera)
+  requestAnimationFrame(render)
+  cx.value = Math.round(camera.position.x)
+  cy.value = Math.round(camera.position.y)
+  cz.value = Math.round(camera.position.z)
+  ox.value = Math.round((controls as any).target.x)
+  oy.value = Math.round((controls as any).target.y)
+  oz.value = Math.round((controls as any).target.z)
+}
+
+// 页面加载完毕执行
+onMounted(() => {
+  Initialize();
+  updateCameraAndRenderer();
+  // Add controller
+  controls = new OrbitControls(camera, container.value)
+  // Damping effect
+  controls.enableDamping = true
+
+  // createVideo()
+  container.value.appendChild(renderer.domElement)
+  render()
+})
+
+
+// Listen for window size change event 监听窗口大小改变事件
+window.addEventListener('resize', function () {
+  test.value++;
+  updateCameraAndRenderer();
+}, false);
+// // 监听鼠标移动事件
+// window.addEventListener('mousemove', function(event) {
+//   // 获取鼠标的位置
+//   var mouseX = event.clientX;
+//   var mouseY = event.clientY;
+
+//   // 将鼠标的位置转换为三维坐标
+//   var vector = new THREE.Vector3(
+//     (mouseX / window.innerWidth) * 2 - 1,
+//     -(mouseY / window.innerHeight) * 2 + 1,
+//     0.5
+//   );
+
+//   vector.unproject(camera);
+
+//   // 设置 OrbitControls 的 target
+//   (controls as any).target.set(vector.x, vector.y, vector.z);
+// });
+
+//测试用前进方法
+const go = () => {
+  var distance = 1;
+  var oriDirection = new THREE.Vector3();
+  camera.getWorldDirection(oriDirection);
+  var direction = new THREE.Vector3(oriDirection.x, 0, oriDirection.z);
+
+  // 计算新的相机位置
+  var newPosition = new THREE.Vector3();
+  newPosition.copy(camera.position); // 开始时，新位置就是相机当前的位置
+  newPosition.add(direction.multiplyScalar(distance)); // 向前移动相机
+
+  // 设置新的相机位置
+  camera.position.copy(newPosition);
+
+  // 计算新的固定点位置
+  var newTarget = new THREE.Vector3();
+  newTarget.copy((controls as any).target); // 开始时，新固定点就是当前的固定点
+  newTarget.add(direction.multiplyScalar(distance)); // 向前移动固定点
+
+  // 设置新的固定点位置
+  (controls as any).target.copy(newTarget);
+}
+
+
+</script>
+<style lang='scss' scoped>
+.container {
+  // height: 100vh;
+  width: 100vw;
+  background-color: #f0f0f0;
+}
+</style>
