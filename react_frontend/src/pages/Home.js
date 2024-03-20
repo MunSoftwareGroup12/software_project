7a1526d56ad1c03f440138471a622cee8593ea08
guest67 <1650202750@qq.com>
2024-03-20 15:46:12 -0230

    add intrudce of the iteration

--- react_frontend/src/pages/Home.js
+++ react_frontend/src/pages/Home.js
@@ -3,15 +3,15 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>...</h1>
+      <h1>Welcome to the ski resort</h1>
       <h3>What has been done in this iteration?</h3>
-      <p>- Created frontend framework based on React+Antd.</p>
-      <p>- Implement 3D map model loading and related environment configuration based on Threejs.</p>
-      <p>- Implement simple responsive design.</p>
-      <h3>What will be done in the next iteration?</h3>
-      <p>- Add more interactive elements to home page.</p>
-      <p>- Display nodes and their associated UIs in the map model.</p>
+      <p>- Add more interactive elements to piste map page.</p>
+      <p>- Display nodes and their associated UIs in the canvas.</p>
       <p>- Implement simple styple of routes search.</p>
+      <h3>What will be done in the next iteration?</h3>
+      <p>- Imporve the routes search.</p>
+      <p>- Improve the page disapaly</p>
     </div>
   );
 }
