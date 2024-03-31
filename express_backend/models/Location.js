637c27e2267708d45ca9f2c2016f3985793ef92a
Nakash Kumar <nakashk510@gmail.com>
2024-03-31 20:40:55 -0230

    seed data into mongodb

---
+++ express_backend/models/Location.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const locationSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    availability: {
+        type: Boolean,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    x: {
+        type: Number,
+        required: true
+    },
+    y: {
+        type: Number,
+        required: true
+    },
+    z: {
+        type: Number,
+        required: true
+    }
+});
+
+const Location = mongoose.model('Location', locationSchema);
+
+module.exports = Location;
