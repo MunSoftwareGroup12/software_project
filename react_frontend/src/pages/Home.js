8c4485b0b962316e3f97fdd0ef52600f45f9e605
guest67 <1650202750@qq.com>
2024-04-04 21:16:38 -0230

    Finish final iteration introduction

--- react_frontend/src/pages/Home.js
+++ react_frontend/src/pages/Home.js
@@ -5,13 +5,11 @@ export default function Home() {
     <div>
       <h1>...</h1>
       <h1>Welcome to the ski resort</h1>
-      <h3>What has been done in this iteration?</h3>
-      <p>- Add more interactive elements to piste map page.</p>
-      <p>- Display nodes and their associated UIs in the canvas.</p>
-      <p>- Implement simple styple of routes search.</p>
-      <h3>What will be done in the next iteration?</h3>
-      <p>- Imporve the routes search.</p>
-      <p>- Improve the page disapaly</p>
+      <h3>What has been done in final iteration?</h3>
+      <p>- The process for the main use case has been completed.</p>
+      <p>- Users can click on the map or click on the search box to determine the start and end locations, and then search for routes.</p>
+      <p>- The system computes multiple feasible routes, and the user can choose a route that exhibits different criteria.</p>
+      <h3>Hoping you are satisfied with our system!</h3>
     </div>
   );
 }
