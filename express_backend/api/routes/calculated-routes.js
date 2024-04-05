1543c451cc0549d672ddddcb7b54a32877468604
Nakash Kumar <nakashk510@gmail.com>
2024-04-05 15:56:19 -0230

    add SkiResort class

--- express_backend/api/routes/calculated-routes.js
+++ express_backend/api/routes/calculated-routes.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const Location = require('../../models/Location');
-const Route = require('../../models/Route');
-const RouteManager = require('../../lib/RouteManager');
-const LocationManager = require('../../lib/LocationManager');
-
+const SkiResort = require('../../lib/SkiResort');
 
 router.get('/', async function (req, res, next) {
     const { startLocationId, endLocationId, difficulty } = req.query;
-    
-    const map = {
-        locations: await Location.find({}),
-        routes: await Route.find({})
-    }
-    const routeManager = new RouteManager(new LocationManager(await Location.find({})));
-    res.json(routeManager.calculateRoutes(map, startLocationId, endLocationId, [...difficulty].map(x => parseInt(x))));
+    const skiResort = new SkiResort()
+    res.json(await skiResort.selectLocation(startLocationId, endLocationId, difficulty));
 });
 
 module.exports = router;
